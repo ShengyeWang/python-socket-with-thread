910fbbd556d4bcfa26d8bbd69bcf573db7b7953c
landpack <ada.frank.ak@gmail.com>
2016-07-15 17:51:57 +0800

    add simple socket server

---
+++ simpleSocketServer.py
@@ -0,0 +1,30 @@
+import socket
+
+class ThreadSocket(object):
+	"""
+	
+	"""
+	def __init__(self, host, port):
+		self.host = host
+		self.port = port
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.sock.bind((self.host, self.port))
+
+	def listen(self):
+		self.sock.listen(5)
+		while True:
+			client, address = self.sock.accept()
+			client.settimeout(60)
+			try:
+				data = client.recv(1024)
+				if data:
+					client.send(data)
+				else:
+					raise error("Client has disconnected")
+			except:
+				client.close()
+			
+if __name__ == '__main__':
+	server=ThreadSocket('',9000)
+	server.listen()
