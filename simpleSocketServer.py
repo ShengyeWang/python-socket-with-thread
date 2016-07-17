5a4530640b2270040ef6cc31b62b5a32285cdfcd
landpack <ada.frank.ak@gmail.com>
2016-07-17 15:57:16 +0800

    add thread local

--- simpleSocketServer.py
+++ simpleSocketServer.py
@@ -2,6 +2,9 @@ import socket
 import threading
 from parser import ClientRequestParser
 
+local = threading.local()
+
+
 class ThreadSocket(object):
 	"""
 	
@@ -25,18 +28,20 @@ class ThreadSocket(object):
 		while True:
 			client, address = self.sock.accept()
 			client.settimeout(60)
-			threading.Thread(target=self.handleClientRequest, args=(client, address)).start()
+			threading.Thread(target=self.handleClientRequest, args=(client, address, local)).start()
 
-	def handleClientRequest(self, client, address):
+	def handleClientRequest(self, client, address,local):
+		local.todo_list = {}
 		while True:
 			try:
 				data = client.recv(1024)
 				if data:
-					response=ClientRequestParser(data=data,db=self.todo_list).response()
+					response=ClientRequestParser(data=data,db=local.todo_list).response()
 					client.send(response)
 				else:
 					raise error("Client has disconnected")
-			except:
+			except Exception as e:
+				print 'Error: ',str(e)
 				client.close()
 		
 if __name__ == '__main__':
